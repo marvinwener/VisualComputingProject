2d85fd41119efbc4901db0ac34bd8fcd2c9e9b20
Maikel Steneker <maikel.steneker@gmail.com>
2015-02-26 11:58:18 +0100

    Added Dummy sensor.

---
+++ src/main/java/nl/tue/win/vcp/virtualbreitenbergenvironment/model/DummySensor.java
@@ -0,0 +1,23 @@
+package nl.tue.win.vcp.virtualbreitenbergenvironment.model;
+
+import javax.media.opengl.GL2;
+import nl.tue.win.vcp.virtualbreitenbergenvironment.utility.Vector;
+
+/**
+ * A sensor that never provides any input. This may be used to essentially
+ * disable a component.
+ *
+ * @author maikel
+ */
+public class DummySensor extends Sensor {
+
+    @Override
+    public float getValue(Vector location, Vector direction) {
+        return 0;
+    }
+
+    @Override
+    public void draw(GL2 gl) {
+    }
+
+}
