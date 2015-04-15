924ea459b563c6eaf1ed21ca3fc3cddeaeb8b6e4
Maikel Steneker <maikel.steneker@gmail.com>
2015-04-15 17:52:34 +0200

    Added field to enable or disable all instability.

--- src/main/java/nl/tue/win/vcp/virtualbreitenbergenvironment/model/sensors/UnstableSensor.java
+++ src/main/java/nl/tue/win/vcp/virtualbreitenbergenvironment/model/sensors/UnstableSensor.java
@@ -11,24 +11,33 @@ import nl.tue.win.vcp.virtualbreitenbergenvironment.utility.Vector;
  * @author maikel
  */
 public class UnstableSensor extends Sensor {
-    
+
     final private Sensor sensor; // the sensor to add randomness to
     final private Random generator; // object that provides randomness
     final private static float SCALE = 0.001f; // extent of randomization
     private final float MIN = 0; // minimum value
     private final float MAX = 1; // maximum value
+    public static boolean ENABLED = true; // whether instability is enabled
 
     public UnstableSensor(Sensor sensor) {
         this.sensor = sensor;
         this.generator = new Random();
     }
-    
+
     @Override
     public float getValue(Vector location, float angle) {
-        final float value = sensor.getValue(location, angle) + getRandomness() * SCALE;
-        if (value > MAX) return MAX;
-        if (value < MIN) return MIN;
-        return value;
+        if (!ENABLED) {
+            return sensor.getValue(location, angle);
+        } else {
+            final float value = sensor.getValue(location, angle) + getRandomness() * SCALE;
+            if (value > MAX) {
+                return MAX;
+            }
+            if (value < MIN) {
+                return MIN;
+            }
+            return value;
+        }
     }
 
     @Override
@@ -39,5 +48,5 @@ public class UnstableSensor extends Sensor {
     protected float getRandomness() {
         return (generator.nextFloat() * 2) - 1;
     }
-    
+
 }
