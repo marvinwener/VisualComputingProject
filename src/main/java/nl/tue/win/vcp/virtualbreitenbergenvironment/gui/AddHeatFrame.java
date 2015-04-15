dc108c9e775fa40230b82e33d56e794dd5d75244
Maikel Steneker <maikel.steneker@gmail.com>
2015-04-15 16:16:48 +0200

    Added window to add heat source (based on adding light source).

---
+++ src/main/java/nl/tue/win/vcp/virtualbreitenbergenvironment/gui/AddHeatFrame.java
@@ -0,0 +1,34 @@
+package nl.tue.win.vcp.virtualbreitenbergenvironment.gui;
+
+import nl.tue.win.vcp.virtualbreitenbergenvironment.model.Environment;
+import nl.tue.win.vcp.virtualbreitenbergenvironment.model.HeatSource;
+import nl.tue.win.vcp.virtualbreitenbergenvironment.utility.Vector;
+
+/**
+ *
+ * @author maikel
+ */
+public class AddHeatFrame extends AddLightFrame {
+
+    public AddHeatFrame(Environment environment) {
+        super(environment);
+    }
+    
+    protected HeatSource getHeatSource() {
+        final Vector position = new Vector((double) getXSpinner().getValue(),
+                (double) getYSpinner().getValue(), (double) getZSpinner().getValue());
+        return new HeatSource(position);
+    }
+
+    @Override
+    protected void addLight() {
+        environment.addHeatSource(getHeatSource());
+        environment.clearPreview();
+    }
+
+    @Override
+    protected void previewLight() {
+        environment.preview(getHeatSource());
+    }
+    
+}
