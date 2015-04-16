016b6aabeebf1c1352dd704f70f604c78377c9fb
Maikel Steneker <maikel.steneker@gmail.com>
2015-04-16 13:57:31 +0200

    Added more tests for collision detection.

--- src/test/java/nl/tue/win/vcp/virtualbreitenbergenvironment/utility/CollisionDetectionTest.java
+++ src/test/java/nl/tue/win/vcp/virtualbreitenbergenvironment/utility/CollisionDetectionTest.java
@@ -15,13 +15,31 @@ public class CollisionDetectionTest {
 
     @Test
     public void testCollision1() {
-        Rectangle a = new Rectangle(new Vector(0.6, 0.8),
+        final Rectangle a = new Rectangle(new Vector(0.6, 0.8),
                 new Vector(-2, 7), new Vector(-8, -1), new Vector(-4, -4), new Vector(2, 4));
-        Rectangle b = new Rectangle(new Vector(-0.6, 0.8),
+        final Rectangle b = new Rectangle(new Vector(-0.6, 0.8),
                 new Vector(3, 0), new Vector(-1, -3), new Vector(5, -11), new Vector(9, -8));
-        boolean expResult = false;
-        boolean result = CollisionDetection.collision(a, b);
+        final boolean expResult = false;
+        final boolean result = CollisionDetection.collision(a, b);
         assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testDetermineDirection() {
+        final Rectangle a = new Rectangle(null,
+                new Vector(-2, 7), new Vector(-8, -1), new Vector(-4, -4), new Vector(2, 4));
+        final Vector expResult = new Vector(0.6, 0.8);
+        final Vector result = a.direction;
+        assertTrue(expResult.equals(result) || expResult.normalized().plus(result.normalized()).length() == 0);
+    }
+    
+    @Test
+    public void testDetermineDirection2() {
+        final Rectangle a = new Rectangle(null,
+                new Vector(3, 0), new Vector(-1, -3), new Vector(5, -11), new Vector(9, -8));
+        final Vector expResult = new Vector(-0.6, 0.8);
+        final Vector result = a.direction;
+        assertTrue(expResult.equals(result) || expResult.normalized().plus(result.normalized()).length() == 0);
     }
 
 }
