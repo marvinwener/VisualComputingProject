d75f12ad5f740537fba2667acd7bc22eae1e6352
Maikel Steneker <maikel.steneker@gmail.com>
2015-04-18 15:25:26 +0200

    Multiple changes in Room.
    - Added DRAW_BOUNDING_BOX and DRAW_WALLS to enable/disable drawing of the walls
      and their bounding boxes;
    - Added code to draw bounding boxes;
    - Added code to get walls (currently not working).

--- src/main/java/nl/tue/win/vcp/virtualbreitenbergenvironment/model/Room.java
+++ src/main/java/nl/tue/win/vcp/virtualbreitenbergenvironment/model/Room.java
@@ -7,6 +7,8 @@ import javax.media.opengl.GL2;
 import static javax.media.opengl.GL2.GL_CURRENT_BIT;
 import static javax.media.opengl.GL2GL3.GL_QUADS;
 import nl.tue.win.vcp.virtualbreitenbergenvironment.model.interfaces.*;
+import nl.tue.win.vcp.virtualbreitenbergenvironment.utility.Rectangle;
+import nl.tue.win.vcp.virtualbreitenbergenvironment.utility.Vector;
 
 /**
  * Represents the walls around the vehicles.
@@ -18,6 +20,8 @@ public class Room implements Drawable {
     private final static float MIN = -10;
     private final static float MAX = 10;
     private final static float STEP = 0.5f;
+    public static boolean DRAW_BOUNDING_BOX = true;
+    public static boolean DRAW_WALLS = false;
 
     @Override
     public void draw(GL2 gl) {
@@ -53,70 +57,102 @@ public class Room implements Drawable {
     }
 
     private void drawWalls(GL2 gl, float min, float max, float step) {
-        gl.glPushAttrib(GL_CURRENT_BIT);
-        gl.glPushMatrix();
+        if (Room.DRAW_WALLS) {
+            gl.glPushAttrib(GL_CURRENT_BIT);
+            gl.glPushMatrix();
 
-        gl.glPopMatrix();
-        gl.glPushMatrix();
-        gl.glTranslated(max, 0, 0);
-        gl.glPushMatrix();
-        gl.glRotated(90, 0, 0, 1);
-        gl.glRotated(90, 1, 0, 0);
-        drawGrid(gl, 0, max, step);
-        gl.glPopMatrix();
-        gl.glPushMatrix();
-        gl.glRotated(-90, 0, 0, 1);
-        gl.glRotated(90, 1, 0, 0);
-        drawGrid(gl, 0, max, step);
-        gl.glPopMatrix();
+            gl.glPopMatrix();
+            gl.glPushMatrix();
+            gl.glTranslated(max, 0, 0);
+            gl.glPushMatrix();
+            gl.glRotated(90, 0, 0, 1);
+            gl.glRotated(90, 1, 0, 0);
+            drawGrid(gl, 0, max, step);
+            gl.glPopMatrix();
+            gl.glPushMatrix();
+            gl.glRotated(-90, 0, 0, 1);
+            gl.glRotated(90, 1, 0, 0);
+            drawGrid(gl, 0, max, step);
+            gl.glPopMatrix();
 
-        gl.glPopMatrix();
-        gl.glPushMatrix();
-        gl.glTranslated(0, max, 0);
-        gl.glPushMatrix();
-        gl.glRotated(90, 1, 0, 0);
-        drawGrid(gl, 0, max, step);
-        gl.glPopMatrix();
-        gl.glPushMatrix();
-        gl.glRotated(180, 0, 0, 1);
-        gl.glRotated(90, 1, 0, 0);
-        drawGrid(gl, 0, max, step);
-        gl.glPopMatrix();
+            gl.glPopMatrix();
+            gl.glPushMatrix();
+            gl.glTranslated(0, max, 0);
+            gl.glPushMatrix();
+            gl.glRotated(90, 1, 0, 0);
+            drawGrid(gl, 0, max, step);
+            gl.glPopMatrix();
+            gl.glPushMatrix();
+            gl.glRotated(180, 0, 0, 1);
+            gl.glRotated(90, 1, 0, 0);
+            drawGrid(gl, 0, max, step);
+            gl.glPopMatrix();
 
-        gl.glPopMatrix();
-        gl.glPushMatrix();
-        gl.glTranslated(min, 0, 0);
-        gl.glPushMatrix();
-        gl.glRotated(90, 0, 0, 1);
-        gl.glRotated(90, 1, 0, 0);
-        drawGrid(gl, 0, max, step);
-        gl.glPopMatrix();
-        gl.glPushMatrix();
-        gl.glRotated(-90, 0, 0, 1);
-        gl.glRotated(90, 1, 0, 0);
-        drawGrid(gl, 0, max, step);
-        gl.glPopMatrix();
+            gl.glPopMatrix();
+            gl.glPushMatrix();
+            gl.glTranslated(min, 0, 0);
+            gl.glPushMatrix();
+            gl.glRotated(90, 0, 0, 1);
+            gl.glRotated(90, 1, 0, 0);
+            drawGrid(gl, 0, max, step);
+            gl.glPopMatrix();
+            gl.glPushMatrix();
+            gl.glRotated(-90, 0, 0, 1);
+            gl.glRotated(90, 1, 0, 0);
+            drawGrid(gl, 0, max, step);
+            gl.glPopMatrix();
 
-        gl.glPopMatrix();
-        gl.glPushMatrix();
-        gl.glTranslated(0, min, 0);
-        gl.glPushMatrix();
-        gl.glRotated(90, 1, 0, 0);
-        drawGrid(gl, 0, max, step);
-        gl.glPopMatrix();
-        gl.glPushMatrix();
-        gl.glRotated(180, 0, 0, 1);
-        gl.glRotated(90, 1, 0, 0);
-        drawGrid(gl, 0, max, step);
-        gl.glPopMatrix();
+            gl.glPopMatrix();
+            gl.glPushMatrix();
+            gl.glTranslated(0, min, 0);
+            gl.glPushMatrix();
+            gl.glRotated(90, 1, 0, 0);
+            drawGrid(gl, 0, max, step);
+            gl.glPopMatrix();
+            gl.glPushMatrix();
+            gl.glRotated(180, 0, 0, 1);
+            gl.glRotated(90, 1, 0, 0);
+            drawGrid(gl, 0, max, step);
+            gl.glPopMatrix();
 
-        gl.glPopMatrix();
-        gl.glPopAttrib();
+            gl.glPopMatrix();
+            gl.glPopAttrib();
+        }
+
+        if (Room.DRAW_BOUNDING_BOX) {
+            gl.glPushAttrib(GL_CURRENT_BIT);
+            gl.glPushMatrix();
+            gl.glColor3f(0, 1, 0);
+            Collection<Collidable> walls = getWalls();
+            for (Collidable wall : walls) {
+                final Rectangle boundingBox = wall.getBoundingBox();
+                gl.glBegin(GL_QUADS);
+                for (Vector corner : boundingBox.corners) {
+                    gl.glVertex2dv(corner.asBuffer());
+                }
+                gl.glEnd();
+            }
+
+            gl.glPopMatrix();
+            gl.glPopAttrib();
+        }
     }
 
     public Collection<Collidable> getWalls() {
-        Collidable[] result = new Collidable[0];
-        // TODO: implement
+        final Collidable[] result = {
+            // left wall
+            new Rectangle.RectangleHolder(new Rectangle(Vector.Y.as2DVector(),
+            new Vector(-10, -10), new Vector(-10, 10), new Vector(-11, 10), new Vector(-11, -10))),
+            // right wall
+            new Rectangle.RectangleHolder(new Rectangle(Vector.Y.as2DVector(),
+            new Vector(11, -10), new Vector(10, -10), new Vector(10, 10), new Vector(11, 10))),
+            // bottom wall
+            new Rectangle.RectangleHolder(new Rectangle(Vector.X.as2DVector(),
+            new Vector(10, -11), new Vector(-10, -11), new Vector(-10, -10), new Vector(10, -10))),
+            // upper wall
+            new Rectangle.RectangleHolder(new Rectangle(Vector.X.as2DVector(),
+            new Vector(10, 11), new Vector(-10, 11), new Vector(-10, 10), new Vector(10, 10)))
+        };
         return Arrays.asList(result);
     }
 }
