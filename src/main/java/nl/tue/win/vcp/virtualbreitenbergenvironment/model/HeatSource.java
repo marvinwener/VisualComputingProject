0b1e4e317d802225124d1736f0dd2093379e2bc1
Maikel Steneker <maikel.steneker@gmail.com>
2015-04-18 19:50:13 +0200

    Heat sources now use textures for a gradient that indicates temperature intensity.

--- src/main/java/nl/tue/win/vcp/virtualbreitenbergenvironment/model/HeatSource.java
+++ src/main/java/nl/tue/win/vcp/virtualbreitenbergenvironment/model/HeatSource.java
@@ -1,7 +1,14 @@
 package nl.tue.win.vcp.virtualbreitenbergenvironment.model;
 
 import com.jogamp.opengl.util.gl2.GLUT;
+import com.jogamp.opengl.util.texture.Texture;
+import com.jogamp.opengl.util.texture.TextureIO;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.media.opengl.GL2;
+import javax.media.opengl.GLException;
 import nl.tue.win.vcp.virtualbreitenbergenvironment.model.sensors.TemperatureSensor;
 import static nl.tue.win.vcp.virtualbreitenbergenvironment.utility.Graphics.drawDisk;
 import nl.tue.win.vcp.virtualbreitenbergenvironment.utility.Vector;
@@ -13,6 +20,11 @@ import nl.tue.win.vcp.virtualbreitenbergenvironment.utility.Vector;
  */
 public class HeatSource extends LightSource {
 
+    private static Texture temperature;
+    private final static String TEXTURE_PATH
+            = "src/main/java/nl/tue/win/vcp/virtualbreitenbergenvironment/"
+            + "graphics/textures/temperature.jpg";
+
     // all functionality is copied from light source, but draw() and toString() are overridden
     public HeatSource(Vector position) {
         super(position);
@@ -20,16 +32,26 @@ public class HeatSource extends LightSource {
 
     @Override
     public void draw(GL2 gl) {
+        if (temperature == null) {
+            try {
+                temperature = TextureIO.newTexture(new File(TEXTURE_PATH), false);
+            } catch (IOException | GLException ex) {
+                Logger.getLogger(HeatSource.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
         GLUT glut = new GLUT();
         gl.glPushMatrix();
         gl.glPushAttrib(GL2.GL_CURRENT_BIT);
         gl.glTranslated(getPosition().x(), getPosition().y(), getPosition().z());
         gl.glPushMatrix();
         gl.glPushAttrib(GL2.GL_CURRENT_BIT);
-        gl.glColor4f(1, 0, 0, 0.3f);
+        gl.glColor4f(1, 1, 1, 0.3f);
         gl.glTranslatef(0, 0, 0.1f);
         gl.glRotatef(90, 0, 1, 0);
+        temperature.enable(gl);
+        temperature.bind(gl);
         drawDisk(gl, TemperatureSensor.DISTANCE_LIMIT, 50);
+        temperature.disable(gl);
         gl.glPopAttrib();
         gl.glPopMatrix();
         super.loadName(gl);
