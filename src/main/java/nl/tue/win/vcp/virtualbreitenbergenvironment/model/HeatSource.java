4bc35855660803f0f1d3ee4710a172e49f29c36b
Maikel Steneker <maikel.steneker@gmail.com>
2015-04-18 22:45:05 +0200

    Fixed heat sources blending together, removed textures.

--- src/main/java/nl/tue/win/vcp/virtualbreitenbergenvironment/model/HeatSource.java
+++ src/main/java/nl/tue/win/vcp/virtualbreitenbergenvironment/model/HeatSource.java
@@ -1,14 +1,7 @@
 package nl.tue.win.vcp.virtualbreitenbergenvironment.model;
 
 import com.jogamp.opengl.util.gl2.GLUT;
-import com.jogamp.opengl.util.texture.Texture;
-import com.jogamp.opengl.util.texture.TextureIO;
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.media.opengl.GL2;
-import javax.media.opengl.GLException;
 import nl.tue.win.vcp.virtualbreitenbergenvironment.model.sensors.TemperatureSensor;
 import static nl.tue.win.vcp.virtualbreitenbergenvironment.utility.Graphics.drawDisk;
 import nl.tue.win.vcp.virtualbreitenbergenvironment.utility.Vector;
@@ -20,40 +13,33 @@ import nl.tue.win.vcp.virtualbreitenbergenvironment.utility.Vector;
  */
 public class HeatSource extends LightSource {
 
-    private static Texture temperature;
     public static boolean DRAW_GRADIENT = true;
     final static String TEXTURE_PATH
             = "src/main/java/nl/tue/win/vcp/virtualbreitenbergenvironment/"
-            + "graphics/textures/temperature.jpg";
+            + "graphics/textures/temperature.png";
+    private static int counter = 0;
+    private final int count;
 
     // all functionality is copied from light source, but draw() and toString() are overridden
     public HeatSource(Vector position) {
         super(position);
+        count = counter++;
     }
 
     @Override
     public void draw(GL2 gl) {
-        if (DRAW_GRADIENT && temperature == null) {
-            try {
-                temperature = TextureIO.newTexture(new File(TEXTURE_PATH), false);
-            } catch (IOException | GLException ex) {
-                Logger.getLogger(HeatSource.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
         GLUT glut = new GLUT();
         gl.glPushMatrix();
         gl.glPushAttrib(GL2.GL_CURRENT_BIT);
         gl.glTranslated(getPosition().x(), getPosition().y(), getPosition().z());
         gl.glPushMatrix();
         gl.glPushAttrib(GL2.GL_CURRENT_BIT);
-        gl.glColor4f(1, 1, 1, 0.3f);
-        gl.glTranslatef(0, 0, 0.1f);
+        gl.glTranslatef(0, 0, 0.1f + 0.05f * count);
         gl.glRotatef(90, 0, 1, 0);
         if (DRAW_GRADIENT) {
-            temperature.enable(gl);
-            temperature.bind(gl);
-            drawDisk(gl, TemperatureSensor.DISTANCE_LIMIT, 50);
-            temperature.disable(gl);
+            gl.glColor4f(1, 0, 0, 0.3f);
+            Vector outerColour = new Vector(1, 0, 0, 0);
+            drawDisk(gl, TemperatureSensor.DISTANCE_LIMIT, 50, outerColour.asBuffer());
         }
         gl.glPopAttrib();
         gl.glPopMatrix();
--- src/main/java/nl/tue/win/vcp/virtualbreitenbergenvironment/utility/Graphics.java
+++ src/main/java/nl/tue/win/vcp/virtualbreitenbergenvironment/utility/Graphics.java
@@ -3,6 +3,7 @@ package nl.tue.win.vcp.virtualbreitenbergenvironment.utility;
 import static java.lang.Math.PI;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
+import java.nio.DoubleBuffer;
 import javax.media.opengl.GL;
 import static javax.media.opengl.GL.GL_LINE_LOOP;
 import javax.media.opengl.GL2;
@@ -45,6 +46,31 @@ public class Graphics {
         gl.glTexCoord1d(1);
         gl.glVertex3d(0, 0, 0);
         gl.glTexCoord1d(0);
+        for (int i = 0; i < slices; i++) {
+            angle = (float) (i * 2 * PI / slices); // compute angle
+            // Define vertex by definition of the unit circle.
+            gl.glVertex3d(0, (cos(angle) * radius), (sin(angle) * radius));
+        }
+        gl.glVertex3d(0, radius, 0);
+        gl.glEnd();
+    }
+
+    /**
+     * Draws the unit circle in a 3D world in the Y and Z directions.
+     *
+     * This version has a gradient from the current colour (inner) to
+     * {@code outerColour}.
+     *
+     * @param gl the graphics object
+     * @param radius the radius of the circle
+     * @param slices the precision of the circle
+     * @param outerColour outer colour of disk (for gradient)
+     */
+    public static void drawDisk(GL2 gl, float radius, int slices, DoubleBuffer outerColour) {
+        float angle; // angle of ray from origin to current point with y axis
+        gl.glBegin(GL.GL_TRIANGLE_FAN);
+        gl.glVertex3d(0, 0, 0);
+        gl.glColor4dv(outerColour);
         for (int i = 0; i < slices; i++) {
             angle = (float) (i * 2 * PI / slices); // compute angle
             // Define vertex by definition of the unit circle.
