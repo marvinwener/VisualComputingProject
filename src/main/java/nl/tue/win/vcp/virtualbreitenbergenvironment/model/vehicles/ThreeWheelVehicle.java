ad839923f04b06df2ae28c6261c361c73cd3946f
Maikel Steneker <maikel.steneker@gmail.com>
2015-04-20 16:22:53 +0200

    Fixed bounding box for three-wheel vehicle.

--- src/main/java/nl/tue/win/vcp/virtualbreitenbergenvironment/model/vehicles/ThreeWheelVehicle.java
+++ src/main/java/nl/tue/win/vcp/virtualbreitenbergenvironment/model/vehicles/ThreeWheelVehicle.java
@@ -3,6 +3,7 @@ package nl.tue.win.vcp.virtualbreitenbergenvironment.model.vehicles;
 import javax.media.opengl.GL2;
 import static javax.media.opengl.GL2.GL_CURRENT_BIT;
 import nl.tue.win.vcp.virtualbreitenbergenvironment.model.sensors.Sensor;
+import nl.tue.win.vcp.virtualbreitenbergenvironment.utility.Rectangle;
 import nl.tue.win.vcp.virtualbreitenbergenvironment.utility.Vector;
 import nl.tue.win.vcp.virtualbreitenbergenvironment.utility.WavefrontObjectLoader_DisplayList;
 
@@ -41,7 +42,7 @@ public class ThreeWheelVehicle extends TwoWheelVehicle {
         }
         gl.glColor3f(1, 0, 0);
         gl.glScaled(SIZE, SIZE, SIZE);
-        gl.glTranslated(0.5, 0.8f, 0);
+        gl.glTranslated(0.5, 0.8, 0);
         gl.glRotated(180, 0, 0, 1);
         gl.glCallList(displayList);
 
@@ -62,5 +63,18 @@ public class ThreeWheelVehicle extends TwoWheelVehicle {
         final Vector toFront = new Vector(0, SIZE, 0).scale(0.85);
         return new Vector[]{wheelVector.plus(toFront),
             wheelVector.scale(-1).plus(toFront)};
+    }
+    
+    @Override
+    public Rectangle getBoundingBox() {
+        final Vector direction = this.getDirection();
+        final Vector wheelDirection = this.getDirection().cross(Vector.Z);
+        
+        final Vector upperRight = position.plus(direction.normalized().scale(SIZE*0.82)).plus(wheelDirection.normalized().scale(0.75));
+        final Vector upperLeft = position.plus(direction.normalized().scale(SIZE*0.82)).minus(wheelDirection.normalized().scale(0.75));
+        final Vector lowerRight = position.minus(direction.normalized().scale(0.22*SIZE)).plus(wheelDirection.normalized().scale(0.75));
+        final Vector lowerLeft = position.minus(direction.normalized().scale(0.22*SIZE)).minus(wheelDirection.normalized().scale(0.75));
+        
+        return new Rectangle(direction, upperRight, upperLeft, lowerLeft, lowerRight);
     }
 }
